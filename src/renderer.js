ef23091c8b568f2f4bad16a6a9092c4017faf07a
Neil <avatarneil@gmail.com>
2017-07-15 22:02:21 -0400

    up collapse is broken :/ working on it!

--- src/renderer.js
+++ src/renderer.js
@@ -50,22 +50,35 @@ document.onkeydown = function(e) {
 function collapseLeft() {
     for (var i = 0; i < currentGrid.length; i++) {
         for (var j = currentGrid[i].length; j > 0; j--) {
-            if (currentGrid[i][j] == currentGrid[i][j - 1]) {
+            if (currentGrid[i][j] == currentGrid[i][j - 1]) { //if same
                 currentGrid[i][j - 1] = (currentGrid[i][j - 1] * 2);
                 currentGrid[i][j] = 0;
-            } else if (currentGrid[i][j - 1] == 0) {
+            } else if (currentGrid[i][j - 1] == 0) { //if left is blank
                 currentGrid[i][j - 1] = currentGrid[i][j];
                 currentGrid[i][j] = 0;
             }
         }
         currentGrid[i] = currentGrid[i].filter(function(element) {
             return element !== undefined;
-        });
+        }); //hackish way to remove undefined elements
     }
 }
 
 function collapseUp() {
-
+    for (var i = currentGrid.length - 1; i > 0; i--) {
+        for (var j = currentGrid[i].length - 1; i > 0; i--) {
+            if (currentGrid[i][j] == currentGrid[i - 1][j]) { //if same
+                currentGrid[i - 1][j] = (currentGrid[i - 1][j] * 2);
+                currentGrid[i][j] = 0;
+            } else if (currentGrid[i - 1][j] == 0) { //if top is blank
+                currentGrid[i - 1][j] = currentGrid[i][j];
+                currentGrid[i][j] = 0;
+            }
+        }
+        currentGrid[i] = currentGrid[i].filter(function(element) {
+            return element !== undefined;
+        }); //hackish way to remove undefined elements
+    }
 }
 
 function collapseRight() {
